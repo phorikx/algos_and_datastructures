a1b8fd2d395b85726c93f0060569a05d6bdb88ce
Paul Horikx <p.horikx@gmail.com>
2024-08-02 20:17:49 +0200

    added merge sort

--- lib/sorting.rb
+++ lib/sorting.rb
@@ -13,4 +13,55 @@ class Sorting
     end
     arr
   end
+
+  def self.merge_sort(arr)
+    merge_sort_starter(arr, 0, arr.length)
+  end
+
+  private
+
+  def self.merge_sort_starter(arr, i, j)
+    return if i >= j
+
+    q = ((i + j) / 2.0).floor
+    merge_sort_starter(arr, i, q)
+    merge_sort_starter(arr, q + 1, j)
+    merge_sort_helper(arr, i, q, j)
+    arr
+  end
+
+  private
+
+  def self.merge_sort_helper(arr, i, j, k)
+    puts "#{arr.length}, #{i}, #{j}, #{k}"
+    length_first_part = j - i
+    length_second_part = k - j
+    arr_1 = arr.slice(i, length_first_part)
+    arr_2 = arr.slice(j, length_second_part)
+    p = 0
+    q = 0
+    r = i
+    while p < length_first_part && q < length_second_part
+      if arr_1[i] < arr_2[q]
+        arr[r] = arr_1[p]
+        p += 1
+      else
+        arr[r] = arr_2[q]
+        q += 1
+      end
+      r += 1
+    end
+    while p < length_first_part
+      arr[r] = arr_1[p]
+      p += 1
+      r += 1
+    end
+    while q < length_second_part
+      arr[r] = arr_2[q]
+      q += 1
+      r += 1
+    end
+    puts(arr)
+    arr
+  end
 end
--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -9,3 +9,11 @@ RSpec.describe Sorting, '#insertion_sort' do
     end
   end
 end
+
+RSpec.describe Sorting, '#merge sort' do
+  context "when given an array that's not sorted" do
+    it 'returns it sorted' do
+      expect(Sorting.merge_sort([3, 2, 1, 4])).to eq([1, 2, 3, 4])
+    end
+  end
+end
